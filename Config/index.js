bfbd2c596363b81c5d460468bc479ce42d6f6892
Iheb haddad <100970145+iheb-haddad@users.noreply.github.com>
2024-12-03 20:33:50 +0100

    Update index.js

--- Config/index.js
+++ Config/index.js
@@ -10,13 +10,13 @@ import 'firebase/compat/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyALgb_TeRJ5sh1DTLf8IFuAWiyjtu1PYmU",
-  authDomain: "whatsappclone-41e72.firebaseapp.com",
-  projectId: "whatsappclone-41e72",
-  storageBucket: "whatsappclone-41e72.firebasestorage.app",
-  messagingSenderId: "403065706030",
-  appId: "1:403065706030:web:b52e2b09a83a8f6d8531bf",
-  measurementId: "G-ZNLFY60QRH"
+  apiKey: "",
+  authDomain: "w",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
